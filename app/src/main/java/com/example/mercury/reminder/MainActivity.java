0d596fc499a4cfb4481d884741c78fdd9ef5a8fe
Intel <jimmdigriz@yandex.ru>
2015-11-28 22:36:37 +0300

    something changes, dont ask me about this

--- app/src/main/java/com/example/mercury/reminder/MainActivity.java
+++ app/src/main/java/com/example/mercury/reminder/MainActivity.java
@@ -8,9 +8,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.UserDictionary;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -25,7 +29,7 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
     private long total;
     private Button btnSave;
-    private EditText editTitle;
+    private AutoCompleteTextView editTitle;
     private EditText editDate;
     private EditText editTime;
     private EditText editDescription;
@@ -57,7 +61,11 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         btnSave = (Button) findViewById(R.id.btnSave);
-        editTitle = (EditText) findViewById(R.id.editTitle);
+        editTitle = (AutoCompleteTextView) findViewById(R.id.editTitle);
+
+        /*editTitle.setAdapter(new ArrayAdapter(this,
+                android.R.layout.simple_dropdown_item_1line, ));*/
+
         editDate = (EditText) findViewById(R.id.editDate);
         editTime = (EditText) findViewById(R.id.editTime);
         editDescription = (EditText) findViewById(R.id.editDescription);
@@ -97,18 +105,27 @@ public class MainActivity extends ActionBarActivity {
 
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        Log.d("TAG", "onTimeSet is beggining");
+                        Log.d("TAG", "onTimeSet is beginning");
+
+                        editTime.setError(null);
+
                         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
                         calendar.set(Calendar.MINUTE, minute);
                         editTime.setText(dtTime.format(calendar.getTime()));
+
+                        if (!isTimeCorrect()) {
+                            editTime.setText("Введено неверное время");
+                            editTime.setError("");
+                        }
                     }
                 }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+
+        timeDialog.updateTime(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE) + 1);
         timeDialog.show();
     }
 
     //диалог с выбором даты
     public void showDatePickerDialog() {
-
         DatePickerDialog dateDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -116,21 +133,18 @@ public class MainActivity extends ActionBarActivity {
                 Log.d("TAG", "onDateSet is beggining");
                     //К сожалению, время вернуть нельзя, и если пользователь захочет это сделать
                  //то мы ему об этом напомним
-                if (calendar.get(Calendar.YEAR) > year ||
-                        calendar.get(Calendar.MONTH) > monthOfYear ||
-                        calendar.get(Calendar.DAY_OF_MONTH) > dayOfMonth) {
+                //очистка от значка ошибки
+                editDate.setError(null);
+
+                //устанавливаем дату выбранную пользователем
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.YEAR, year);
+                editDate.setText(dtDate.format((calendar.getTime())));
+
+                if (!isTimeCorrect()){
                     editDate.setText("Введена неверная дата");
                     editDate.setError("");
-                    return;
-                }else {
-                    //очистка от значка ошибки
-                    editDate.setError(null);
-
-                    //устанавливаем дату выбранную пользователем
-                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                    calendar.set(Calendar.MONTH, monthOfYear);
-                    calendar.set(Calendar.YEAR, year);
-                    editDate.setText(dtDate.format((calendar.getTime())));
                 }
             }
 
@@ -138,26 +152,31 @@ public class MainActivity extends ActionBarActivity {
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         calendar.getTimeInMillis();
         dateDialog.show();
+    }
 
+    private boolean isTimeCorrect() {
+        return !(System.currentTimeMillis() >= calendar.getTimeInMillis());
     }
 
     //пользователь сохраняет введенные данные
     //производим валидацию на отсутствие даты\времени
     private void saveAndValidateInformation() {
-
-
-        if (editDate.length() == 0) {
-            editDate.setError("Вы должны указать дату");
+        if (editDate.length() == 0 || editDate.getText().toString().contentEquals("Введена неверная дата")) {
+            //editDate.setError("Вы должны указать дату");
+            Toast.makeText(this, "Вы должны указать дату", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if (editTime.length() == 0) {
-            editTime.setError("Вы должны указать время");
+        if (editTime.length() == 0 || editTime.getText().toString().contentEquals("Введено неверное время")) {
+            //editTime.setError("Вы должны указать время");
+            Toast.makeText(this, "Вы должны указать время", Toast.LENGTH_SHORT).show();
+            //Snackbar.make(findViewById(R.id.Main), "kokokkokokoko", Snackbar.LENGTH_SHORT).show();
             return;
         }
 
         if (editTitle.length() == 0) {
-            editTitle.setError("Вы должны ввести заголовок напоминания");
+            //editTitle.setError("Вы должны ввести заголовок напоминания");
+            Toast.makeText(this, "Вы должны ввести заголовок напоминания", Toast.LENGTH_SHORT).show();
             return;
         }
         //валидация пройдена, сохраняем  значения
@@ -166,10 +185,10 @@ public class MainActivity extends ActionBarActivity {
         editor.putString("time", editTime.getText().toString());
         editor.putString("description", editDescription.getText().toString());
         editor.apply();
+
         total = calendar.getTimeInMillis();
         Toast.makeText(getApplicationContext(), "Напоминание сохранено", Toast.LENGTH_SHORT).show();
         prepareNotification();
-
     }
 
     //кладем имплисити интент в аларм менеджер, который вызовет его в указанное пользователем время.
